f920ad3eb6678da7d200b61de0c027059a1aa991
Shoaib <shoaibs411@gmail.com>
2024-01-24 21:00:03 +0530

    TF Autopush Updated alb Module remote state name revert

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket  = "b56-terraform-state--bucket"
-    key     = "${var.ENV}/vpc/terraform.tfstate"
+    key     = "vpc/${var.ENV}/terraform.tfstate"
     region  = "us-east-1"
   }
 }
